600a71a50b2347be676d0af177242f7d4c57674e
Ajith-NM <ajith.n.m5603@gmail.com>
2025-02-13 10:28:44 +0530

    parallax website

---
+++ parallax/script.js
@@ -0,0 +1,17 @@
+var bg1 = document.getElementById("bg1");
+var bg2 = document.getElementById("bg2");
+var cloud_l = document.getElementById("cloud_left");
+var cloud_r = document.getElementById("cloud_right");
+var mountain = document.getElementById("mountain");
+var sun = document.getElementById("sun");
+var tree_l = document.getElementById("tree_left");
+var tree_r = document.getElementById("tree_right");
+
+window.addEventListener("scroll", () => {
+  let value = window.scrollY;
+  tree_l.style.left = value * -1.5 + "px";
+  tree_r.style.left = value * 1.5 + "px";
+  cloud_l.style.left = value * -1.5 + "px";
+  cloud_r.style.left = value * 1.5 + "px";
+  sun.style.top = value * -0.5 + "px";
+});
